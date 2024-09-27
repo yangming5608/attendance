691076b693e361c0cedf6882c776b96a8cb68309
杨明 <798187056@qq.com>
2024-09-27 10:14:51 +0800

    first

--- doc/attendance.sql
+++ doc/attendance.sql
@@ -5,7 +5,7 @@
  Source Server Type    : MySQL
  Source Server Version : 80019
  Source Host           : localhost:3306
- Source Schema         : attendance_manager
+ Source Schema         : attendance
 
  Target Server Type    : MySQL
  Target Server Version : 80019
